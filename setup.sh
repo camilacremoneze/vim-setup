9e18e5827925429f5a2fac959021cce11a9d555a
camilacremoneze <camilacremoneze@gmail.com>
2020-03-05 14:23:26 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/env bash
 
-#post_install
-#:PluginInstall
-  
-#install vundle 
-#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-#install vim ruby
-#sudo apt-get install -y ruby rubygems vim-nox
-#Recompile command-t
-#cd ~/.vim/ruby/command-t
-#  ruby extconf.rb
-#    make
-
 install() {
   #ctags
   sudo apt-get install ctags
